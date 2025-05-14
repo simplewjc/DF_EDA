8cd48a7cbc50ada512236a4df4c92a2f9cea7d40
simplewjc <328775664@qq.com>
2025-05-14 10:39:37 +0800

    add png and modify md

--- README.md
+++ README.md
@@ -3,14 +3,16 @@
 ## 参赛信息
 
 Author name: 汪建成
+
 Author school: 吉林大学
+
 Author email: wjc24@mails.jlu.edu.com
 
 ## 目录说明
 
-/ckpt 目录提供本组进行的六组实验的关键 pth 权重文件,其中 ex5_checkpoint_epoch_39.pth 为最佳模型权重文件
+/ckpt 目录提供本组进行的六组实验的关键 pth 权重文件,其中 ex5_checkpoint_epoch_39.pth 为最佳模型权重文件 （镜像中提供）
 /df 目录包含比赛相关的测试及分析代码，包含 get_gt_data.py 和 eval_gt_and_pred.py 文件
-/df_pred_reult 目录为比赛相关的结果分析文件，包含 testing_B1 的预测结果 result_processed_scenarios_testing_B1_part.pkl 文件
+/df_pred_reult 目录为比赛相关的结果分析文件，包含 testing_B1 的预测结果 result_processed_scenarios_testing_B1_part.pkl 文件（镜像中提供）
 /docs 为说明文档目录
 /eda 及 /MTR 为项目模型实现代码
 /output 为 train/val/test/pred/ 等默认输出路径
@@ -21,7 +23,7 @@ Author email: wjc24@mails.jlu.edu.com
 算法测试说明文档见 [算法测试说明文档](算法测试说明文档.md)
 主要包含 项目总体介绍、预测结果快速验证（基于 Python）、预测结果完整验证（基于 Docker）、项目完整复现流程（基于 Docker） 内容
 
-其中 预测结果快速验证（基于 Python）、预测结果完整验证（基于 Docker）可以验证对 processed_scenarios_testing_B1_part 目录的预测效果，官方提供 part (1s) 历史信息，这里根据 1s 信息预测出未来 8s 的结果文件为：[result_processed_scenarios_testing_B1_part.pkl](df_pred_result/result_processed_scenarios_testing_B1_part.pkl)
+其中 预测结果快速验证（基于 Python）、预测结果完整验证（基于 Docker）可以验证对 processed_scenarios_testing_B1_part 目录的预测效果，官方提供 part (1s) 历史信息，这里根据 1s 信息预测出未来 8s 的结果文件为：[result_processed_scenarios_testing_B1_part.pkl](df_pred_result/result_processed_scenarios_testing_B1_part.pkl)（镜像中提供）
 需要官方后续进行评测，详细步骤见算法测试说明文档
 
 ## 算法说明文档
--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
-# # 预测结果快速验证
+# 预测结果完整验证(example)
 # docker run --gpus all \
 #   -it \
 #   --memory=32g --memory-swap=60g \
@@ -8,7 +8,8 @@
 #   simplewjc/df_eda_image:v3 \
 #   bash
 
-# 项目完整复现流程
+
+# 项目完整复现流程 (example)
 docker run --gpus all \
   -it \
   --memory=30g --memory-swap=60g \
@@ -16,7 +17,37 @@ docker run --gpus all \
   -v /media/simple/Data/waymo_open_dataset_motion_v_1_1_0:/root/workspace/Dataset/waymo_open_dataset_motion_v_1_1_0 \
   -v /media/simple/Data/Dataset/Waymo_Dataset:/root/workspace/Dataset/Waymo_Dataset \
   -v /media/simple/Data/Dataset/DF_Dataset:/root/workspace/Dataset/DF_Dataset \
-  -v /media/simple/Data/WorkSpace/DF_EDA:/root/workspace/DF_EDA \
-  -v /media/simple/Data/WorkSpace/EX_CKPT:/root/workspace/EX_CKPT \
+  -v /home/DF_EDA:/root/workspace/DF_EDA \
   simplewjc/df_eda_image:v3 \
   bash
+
+
+#-----------------------------------------------------------------------------------------
+
+# 容器内预测结果完整验证 docker 启动脚本
+# 本参赛组提供结果镜像为 simplewjc/df_eda_image:v3 ，使用上述镜像启动 Docker容器(合理设置 YOUR_DF_Dataset_PATH,YOUR_PROJECT_PATH)，启动容器命令为:
+# 详情见 算法测试说明文档.md
+
+# docker run --gpus all \
+#   -it \
+#   --memory=30g --memory-swap=60g \
+#   --shm-size=30g \
+#   -v YOUR_DF_Dataset_PATH/DF_Dataset:/root/workspace/Dataset/DF_Dataset \
+#   -v YOUR_PROJECT_PATH/DF_EDA:/root/workspace/DF_EDA \
+#   simplewjc/df_eda_image:v3 \
+#   bash
+
+
+# 容器内项目完整复现流程 docker 启动脚本
+# 本参赛组提供镜像为 simplewjc/df_eda_image:v3，使用上述镜像启动 Docker容器(合理设置 YOUR_DF_Dataset_PATH,YOUR_WOMD_PATH,YOUR_Waymo_Dataset_PATH,YOUR_PROJECT_PATH):
+# 详情见 算法测试说明文档.md
+
+# docker run --gpus all \
+#   -it \
+#   --memory=32g --memory-swap=60g \
+#   -v YOUR_WOMD_PATH/waymo_open_dataset_motion_v_1_1_0/scenario:/root/workspace/Dataset/waymo_open_dataset_motion_v_1_1_0/scenario \
+#   -v YOUR_Waymo_Dataset_PATH/Waymo_Dataset:/root/workspace/Dataset/Waymo_Dataset \
+#   -v YOUR_DF_Dataset_PATH/DF_Dataset:/root/workspace/Dataset/DF_Dataset \
+#   -v YOUR_PROJECT_PATH/DF_EDA:/root/workspace/DF_EDA \
+#   simplewjc/df_eda_image:v3 \
+#   bash
